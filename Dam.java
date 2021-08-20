c6cfdcee0d29840a99713f21389d091da9e075b1
tmanbarton <54116906+tmanbarton@users.noreply.github.com>
2021-08-19 19:36:01 -0600

    refactor directions to more closely resemble Fairplay and the land I'm thinking of

--- Dam.java
+++ Dam.java
@@ -14,9 +14,9 @@ public class Dam extends Location {
     // Turn wheel if magnet has been dropped
     public void turn(Location location) {
         if(((Dam)location).magnetDropped) {
-            System.out.println("The ground starts to rumble and you see a massive concrete wall start to rise out of the water on\nthe opposite side of the lake, blocking the flow of water from the river into the lake. There's\nanother shudder and a huge whirl pool form near the middle of the lake and the water level starts\ngoing down. Soon the water is completely gone, revealing a town that had been under water only a\nfew minutes ago. You can probably get to the town if you go down the dam to the east.");
+            System.out.println("The ground starts to rumble and you see a massive concrete wall start to rise out of the water on\nthe opposite side of the lake, blocking the flow of water from the river into the lake. There's\nanother shudder and a huge whirl pool form near the middle of the lake and the water level starts\ngoing down. Soon the water is completely gone, revealing a town that had been under water only a\nfew minutes ago. You can probably get to the town if you go down the dam to the west.");
             Main m = new Main();
-            location.connectingLocations.get(3).directions = m.east;
+            location.connectingLocations.get(3).directions = m.west;
             location.connectingLocations.get(4).directions = m.down;
         }
         else {
